7bab07df477c267fd39cd3bd566340527897fb1f
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2023-01-09 17:33:07 +0100

    Begin conversion FindWayToAntColony to c++

---
+++ Source/AntAlgorithms/Private/FindWayToAntColony.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FindWayToAntColony.h"
+
+EBTNodeResult::Type UFindWayToAntColony::ExecuteTask(UBehaviorTreeComponent& OwnerComponent, uint8* NodeMemory)
+{
+    TArray<AActor*> Actors;
+
+    return EBTNodeResult::Type();
+}
