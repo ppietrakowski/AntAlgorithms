64eb0ba99be429ee8661ddd1569f04c418def78b
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2023-01-09 17:36:23 +0100

    Implementing searching for Ant Colony

--- Source/AntAlgorithms/Private/FindWayToAntColony.cpp
+++ Source/AntAlgorithms/Private/FindWayToAntColony.cpp
@@ -2,10 +2,25 @@
 
 
 #include "FindWayToAntColony.h"
+#include "AntColony.h"
+#include "Kismet/GameplayStatics.h"
+#include "AntColonySimulation.h"
+#include <BehaviorTree/BlackboardComponent.h>
 
 EBTNodeResult::Type UFindWayToAntColony::ExecuteTask(UBehaviorTreeComponent& OwnerComponent, uint8* NodeMemory)
 {
     TArray<AActor*> Actors;
 
-    return EBTNodeResult::Type();
+    UGameplayStatics::GetAllActorsWithInterface(this, UAntColony::StaticClass(), Actors);
+
+    if (Actors.IsEmpty())
+    {
+        return EBTNodeResult::Failed;
+    }
+
+    UBlackboardComponent* Blackboard = OwnerComponent.GetBlackboardComponent();
+
+    Blackboard->SetValueAsVector(OutPositionKey.SelectedKeyName, Actors[0]->GetActorLocation());
+
+    return EBTNodeResult::Succeeded;
 }
